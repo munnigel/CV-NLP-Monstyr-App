5c382b3473ee3ff7028ca62e4e037337cdbe1bcf
Claudiapotato <claudia_lai@mymail.sutd.edu.sg>
2022-07-08 17:42:06 +0800

    commit

--- backend/features/steps.rb
+++ backend/features/steps.rb
@@ -1,27 +1,36 @@
-Given('I am on the live post edit page') do |click|
-    visit('/live_posts/' + click) 
 
-    
-    end
-    
-  end
+
+Given('I am on the login page') do
+    visit('/login')
+
+end
+
+When('I click on the {string} button') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be logged in to the Admin Dashboard') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+
+#     Given('I click on live post {string}') do |string|
+#     @live_post = @live_posts.find { |post| post.text == string }
+#     @live_post.click
+#     end
+# # Given('I click on live post number \{int}') do |int|
+# #     @live_post = @live_posts[int]
+# #     @live_post.click 
+# #     end
+
+# #   end
   
-  Given('I click a post to edit') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+#   When('I click on edit button') do
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
   
-  When('I edit the post') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+#   Then('I should see edit live post page') do
+#     pending # Write code here that turns the phrase above into concrete actions
+#   end
   
-  When('I click the Submit button') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then('I should be brought to the live post edit page') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then('I should see the post I edited') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
