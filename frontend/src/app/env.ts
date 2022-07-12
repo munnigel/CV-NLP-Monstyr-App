e290820e78ada0eb41e4240cba30d8040f659871
Abram Tan <72917689+abramtan@users.noreply.github.com>
2022-07-13 03:54:40 +0800

    Update for deployment
    Changed files for GCP deployment.

--- frontend/src/app/env.ts
+++ frontend/src/app/env.ts
@@ -1,3 +1,3 @@
-// export const API_URL =
-//   'https://rubyduckies-rails-backend-wmma3ffcjq-as.a.run.app';
-export const API_URL = 'http://localhost:3000';
+export const API_URL =
+  'https://rubyduckies-rails-backend-wmma3ffcjq-as.a.run.app';
+// export const API_URL = 'http://localhost:3000';
