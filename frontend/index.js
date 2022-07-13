96ec00113773241d8c4d2117b8ef21ac6509528c
Wang Lin <64834284+99wanglin@users.noreply.github.com>
2022-07-14 01:21:15 +0800

    fix index.js to accept all routes

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express()
 
 app.use(express.static("./dist/monstyr-app"));
-app.get('/[^\.]+$', function(req, res){
+app.get('*', function(req, res){
     res.set('Content-Type', 'text/html')
         .sendFile(__dirname + '/dist/monstyr-app/index.html');})
 
