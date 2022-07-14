ec9c4d284e05f0ef7931ecaf23ba96d7a9e86bdb
s4nat <65476084+s4nat@users.noreply.github.com>
2022-07-14 17:14:49 +0800

    cypress for dateparsing works

--- frontend/cypress/integration/dateparser/dateparser_step.js
+++ frontend/cypress/integration/dateparser/dateparser_step.js
@@ -11,7 +11,7 @@ When("I click on the Start Date Generate", () => {
 });
 
 Then("the start date of the promotion should appear in the \"Start Date\" text box", () => {
-    cy.get("#startText").contains(/[a-zA-Z0-0]/)
+    cy.get("#startText").should("be.visible")
 });
 
 
@@ -20,30 +20,30 @@ Given("I am on \"Edit Item Page\" for a particular pending post", () => {
     cy.visit("http://localhost:4200/edit/2")
 });
 
-When("I click on the \"End Date\"'s \"Generate\"", () => {
+When("I click on the End Date Generate", () => {
     cy.get("#endGen").click({ force: true })
 });
 
-Then("the start date of the promotion should appear in the \"Start Date\" text box", () => {
-    cy.get("#endText").contains(/[a-zA-Z0-0]/)
+Then("the end date of the promotion should appear in the \"End Date\" text box", () => {
+    cy.get("#endText").should("be.visible")
 });
 
 
-Given("I am on \"Edit Item Page\" for a particular pending post", () => {
+Given("I am on \"Edit Item Page\" for a particular pending post with no date", () => {
     cy.visit("http://localhost:4200/edit/12")
 });
 
-When("I click on the \"Start Date\"'s \"Generate\"", () => {
+When("I click on the Start Date Generate", () => {
     cy.get("#startGen").click({ force: true })
 });
 
-Then("the the \"Start Date\" text box should remain empty", () => {
+Then("the Start Date text box should remain empty", () => {
     cy.get("#startText").should('have.value', 'Invalid Date')
 });
 
 
 
-Given("I am on \"Edit Item Page\" for a particular pending post", () => {
+Given("I am on \"Edit Item Page\" for a particular pending post with no date", () => {
     cy.visit("http://localhost:4200/edit/12")
 });
 
