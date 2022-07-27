cb000dd63ec81f869dd228c1983415358a0c7bb2
99wanglin <wanglinsim@gmail.com>
2022-07-27 21:55:04 +0800

    display tags and categories on live posts

--- frontend/cypress/integration/tags/tags_steps.js
+++ frontend/cypress/integration/tags/tags_steps.js
@@ -1,32 +1,32 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-
 // Scenario: Generating tags when post has an attached image
 
-Given("I am on \"Edit Item Page\" for a particular pending post", () => {
-    cy.visit("http://localhost:4200/edit/4")
+Given('I am on "Edit Item Page" for a particular pending post', () => {
+  cy.visit("http://localhost:4200/home/pending");
+  cy.get(".pending-posts-item").first().click();
 });
 
 And("the pending post has an attached image", () => {
-    cy.get("img").should("exist")
+  cy.get("img").should("exist");
 });
 
-When("I click on the \"generate tags\" button", () => {
-    cy.get("#generate-tags-btn").click({ force: true })
+When('I click on the "generate tags" button', () => {
+  cy.get("#generate-tags-btn").click({ force: true });
 });
-And("I click on the \"enter tags\" text area", () => {
-    cy.get("#tags-box").click({ force: true })
+And('I click on the "enter tags" text area', () => {
+  cy.get("#tagInput").click({ force: true });
 });
 
 Then("I should see a dropdown list of tags", (optionSelector) => {
-      cy.get("mat-option-text").should("exist")
-      });
-
-    // Scenario: Generating tags when post has no attached image
-Given("I see a dropdown list of tags", (optionSelector) => {
-    cy.get("#mat-option-text").first().click({force:true})
+  cy.get("mat-option-text").should("exist");
 });
 
-When ("I click on the suitable tags related to the post", () => {
-    cy.get(".tags-options").click({ force: true })
+// Scenario: Generating tags when post has no attached image
+Given("I see a dropdown list of tags", (optionSelector) => {
+  cy.get("#mat-option-text").first().click({ force: true });
+});
+
+When("I click on the suitable tags related to the post", () => {
+  cy.get(".tags-options").click({ force: true });
 });
