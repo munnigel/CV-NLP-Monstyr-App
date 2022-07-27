ebe514afd825289fd7f3f665a2bc7aefe79a5df2
Claudiapotato <93199737+Claudiapotato@users.noreply.github.com>
2022-07-27 23:43:49 +0800

    yess

--- frontend/cypress/integration/tags.feature
+++ frontend/cypress/integration/tags.feature
@@ -13,21 +13,25 @@ Scenario: Generating tags when post has an attached image
 Scenario: Accept tag
     Given I see a dropdown list of tags
     When I click on a suitable tag related to the post
-    Then the tag should be inserted into the "enter tags" text area
-    
-Scenario: Attempting to generate tags when post has no attached image
-    Given I am on "Edit Item Page" for a particular pending post
-    And the pending post has no attached image
-    When I click on the "generate tags" button
-    Then I should see no tags in the dropdown list
+    Then Then I should not see a dropdown list of tags
+    And the tag should be inserted into the "enter tags" text area
 
 Scenario: Creating a tag not present in generated suggestions
     Given I am on "Edit Item Page" for a particular pending post
-    When I click on the "enter tag" text area
-    And type a new tag
-    Then a new tag is added to the dropdown list of tags
+    When I click on the "enter tags" text area
+    When I type a new tag and press the "enter" key
+    Then the new tag should be inserted into the "enter tags" text area
 
 Scenario: Saving tags to post
     Given I am on "Edit Item Page" for a particular pending post
     And I click the "submit" button
     Then the accepted tags should be added to the post
+
+
+
+
+Scenario: Attempting to generate tags when post has no attached image
+    Given I am on "Edit Item Page" for a particular pending post
+    And the pending post has no attached image
+    When I click on the "generate tags" button
+    Then I should see no tags in the dropdown list
--- frontend/cypress/integration/tags/tags_steps.js
+++ frontend/cypress/integration/tags/tags_steps.js
@@ -3,7 +3,7 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 // Scenario: Generating tags when post has an attached image
 
 Given("I am on \"Edit Item Page\" for a particular pending post", () => {
-    cy.visit("http://localhost:4200/home/pending");
+  cy.visit("http://localhost:4200/home/pending");
   cy.get(".pending-posts-item").first().click();
 });
 
@@ -20,7 +20,7 @@ And("I click on the \"enter tags\" text area", () => {
 
 Then("I should see a dropdown list of tags", (optionSelector) => {
       cy.get("#tags-options").should("exist")
-      });
+});
 
 
 
@@ -35,5 +35,60 @@ When ("I click on a suitable tag related to the post", () => {
 });
 
 Then("the tag should be inserted into the \"enter tags\" text area", () => {
-    cy.get("#tags-options").first().should("not.exist")
+    cy.get("#tags-options").should("not.exist")
 });
+
+
+// Creating a tag not present in generated suggestions
+Given("I am on \"Edit Item Page\" for a particular pending post", () => {
+    cy.visit("http://localhost:4200/home/pending");
+  cy.get(".pending-posts-item").first().click();
+});
+
+When ("I click on the \"enter tags\" text area", () => {
+    cy.get("#tagInput").click({ force: true })
+});
+
+When ("I type a new tag and press the \"enter\" key", () => {
+    cy.get("#tagInput").type("new tag").type("{enter}")
+});
+
+Then ("the new tag should be inserted into the \"enter tags\" text area", (optionSelector) => {
+    cy.get("#tags-options").should("exist")
+});
+
+//Saving tags to post
+Given("I am on \"Edit Item Page\" for a particular pending post", () => {
+    cy.visit("http://localhost:4200/home/pending");
+    cy.get(".pending-posts-item").first().click();
+});
+
+And ("I click on the \"submit\" button", () => {
+    cy.get("#submit-btn").click({ force: true })
+});
+
+Then ("the accepted tags should be added to the post", () => {
+    cy.get(".accepted-tags").should("exist")
+});
+
+
+
+
+
+
+//Attempting to generate tags when post has no attached image
+
+Given("I am on \"Edit Item Page\" for a particular pending post", () => {
+    cy.visit("http://localhost:4200/home/pending");
+  cy.get(".pending-posts-item").first().click();
+});
+
+And("the pending post has no attached image", () => {
+    cy.get("img").should("not.exist");
+    });
+
+When('I click on the "generate tags" button', () => {
+    cy.get("#generate-tags-btn").click({ force: true });
+    });
+
+
