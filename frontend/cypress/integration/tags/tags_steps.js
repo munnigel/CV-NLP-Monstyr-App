7919fb7564a7078b5814d2f25b52ffd831c7d0ae
s4nat <65476084+s4nat@users.noreply.github.com>
2022-07-28 01:44:12 +0800

    changed categories feature file

--- frontend/cypress/integration/tags/tags_steps.js
+++ frontend/cypress/integration/tags/tags_steps.js
@@ -12,7 +12,7 @@ And("the pending post has an attached image", () => {
 });
 
 When('I click on the "generate tags" button', () => {
-  cy.get("#generate-tags-btn").click({ force: true });
+  cy.get(".generate-tags-btn").click({ force: true });
 });
 And('I click on the "enter tags" text area', () => {
   cy.get(".tagInput").click({ force: true });
