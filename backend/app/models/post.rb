0ed1af7430c66b28d1d44bc728ade56afa2b034b
Abram Tan <72917689+abramtan@users.noreply.github.com>
2022-08-04 22:46:21 +0800

    Post status validation
    Checks if status attribute meets criteria.

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
     has_one_attached :image
+
+    APPROVED_STATUS = ["pending", "live"]
+
+    validates :status, presence: true, if: :status_check
+    
+    private
+    def status_check
+        unless APPROVED_STATUS.any? { |word| status == word}
+        errors.add(:status, "is not a valid status. Status can only be #{APPROVED_STATUS}")
+        end
+    end
 end
