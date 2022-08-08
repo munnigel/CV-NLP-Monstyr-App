6b38e8dbf47a065a902d4519261a30d7970e7c33
99wanglin <wanglinsim@gmail.com>
2022-08-08 17:19:40 +0800

    update views for edit live post

--- frontend/src/app/edit-live-post/edit-live-post.component.ts
+++ frontend/src/app/edit-live-post/edit-live-post.component.ts
@@ -26,7 +26,6 @@ import { MatChipInputEvent } from '@angular/material/chips';
 })
 export class EditProcessedPostComponent implements OnInit {
   product: Product;
-  editForm: UntypedFormGroup;
   error = false;
   errMsg: string;
   id: number;
@@ -39,6 +38,24 @@ export class EditProcessedPostComponent implements OnInit {
   addOnBlur = true;
 
   datePicker: UntypedFormGroup;
+
+  formats = [
+    ['New!', '<Product Name>'],
+    ['New Outlet!', '<Location>', '<Unit Number>'],
+    ['X%', 'OFF', '<Product Name>'],
+    ['Up to', 'X%', 'OFF', '<Product Name>'],
+    ['X', '-for-', 'Y', '<Product Name>'],
+    ['$', '<Amount>', 'for', '<Product Name>'],
+    ['Free', '<Product Name>'],
+  ];
+  selectedFormat: number;
+  finalTitle: string;
+  titleProductName: string;
+  titleLocation: string;
+  titleXOFF: string;
+  titleXForY: string;
+  titleUnitNumber: string;
+  titleAmount: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -58,29 +75,117 @@ export class EditProcessedPostComponent implements OnInit {
       this.id = params['id'];
       // console.log(this.datasrv.getPendingProductList()[id]);
       let productList = this.datasrv.getLiveProductList();
-      for (let product of productList) {
-        if (product.id == this.id) {
-          this.product = product;
-          break;
+      if (!productList) {
+        let output: any;
+        this.datasrv.getProductInfo(this.id).subscribe({
+          next: (res) => {
+            output = res;
+          },
+          error: () => {
+            console.log('error getting product');
+          },
+          complete: () => {
+            this.product = this.datasrv.createAndStoreProduct(output);
+            if (this.product.title) this.titleCtrl.setValue(this.product.title);
+            if (this.product.content)
+              this.descriptionCtrl.setValue(this.product.content);
+            if (this.product.tags) this.tags = this.product.tags;
+            if (this.product.categories)
+              this.categories = this.product.categories;
+            if (this.product.startDate)
+              this.datePicker.patchValue({ start: this.product.startDate });
+            if (this.product.endDate)
+              this.datePicker.patchValue({ end: this.product.endDate });
+          },
+        });
+      } else
+        for (let product of productList) {
+          if (product.id == this.id) {
+            this.product = product;
+            break;
+          }
         }
-      }
       console.log(this.product);
-      this.editForm = this.fb.group({
-        title: [`${this.product.title}`, Validators.required],
-        content: [`${this.product.content}`, Validators.required],
-        startDate: [`${this.product.startDate}`, Validators.required],
-        endDate: [`${this.product.endDate}`, Validators.required],
-      });
+      if (this.product.title) this.titleCtrl.setValue(this.product.title);
+      if (this.product.content)
+        this.descriptionCtrl.setValue(this.product.content);
+      if (this.product.tags) this.tags = this.product.tags;
+      if (this.product.categories) this.categories = this.product.categories;
+      if (this.product.startDate)
+        this.datePicker.patchValue({ start: this.product.startDate });
+      if (this.product.endDate)
+        this.datePicker.patchValue({ end: this.product.endDate });
     });
-    if (this.product.title) this.titleCtrl.setValue(this.product.title);
-    if (this.product.content)
-      this.descriptionCtrl.setValue(this.product.content);
-    if (this.product.tags) this.tags = this.product.tags;
-    if (this.product.categories) this.categories = this.product.categories;
-    if (this.product.startDate)
-      this.datePicker.patchValue({ start: this.product.startDate });
-    if (this.product.endDate)
-      this.datePicker.patchValue({ end: this.product.endDate });
+  }
+
+  updateProductName(event) {
+    // console.log(event.target.value);
+    this.titleProductName = event.target.value;
+    this.updateFinalString();
+  }
+
+  updateAmount(event) {
+    // console.log(event.target.value);
+    this.titleAmount = event.target.value;
+    this.updateFinalString();
+  }
+
+  updateXForY(event) {
+    // console.log(event.target.value);
+    this.titleXForY = event.target.value;
+    this.updateFinalString();
+  }
+
+  updateXOFF(event) {
+    // console.log(event.target.value);
+    this.titleXOFF = event.target.value;
+    this.updateFinalString();
+  }
+
+  updateUnitNumber(event) {
+    // console.log(event.target.value);
+    this.titleUnitNumber = event.target.value;
+    this.updateFinalString();
+  }
+
+  updateLocation(event) {
+    // console.log(event.target.value);
+    this.titleLocation = event.target.value;
+    this.updateFinalString();
+  }
+
+  selectFormat(i: number) {
+    this.resetFields();
+    this.selectedFormat = i;
+    this.updateFinalString();
+  }
+
+  resetFields() {
+    this.titleProductName = '';
+    this.titleLocation = '';
+    this.titleXOFF = '';
+    this.titleXForY = '';
+    this.titleUnitNumber = '';
+    this.titleAmount = '';
+  }
+
+  updateFinalString() {
+    if (this.selectedFormat == 0) {
+      this.finalTitle = 'New! ' + this.titleProductName;
+    } else if (this.selectedFormat == 1) {
+      this.finalTitle =
+        'New Outlet! ' + this.titleLocation + ', ' + this.titleUnitNumber;
+    } else if (this.selectedFormat == 2) {
+      this.finalTitle = this.titleXOFF + ' ' + this.titleProductName;
+    } else if (this.selectedFormat == 3) {
+      this.finalTitle = 'Up to ' + this.titleXOFF + ' ' + this.titleProductName;
+    } else if (this.selectedFormat == 4) {
+      this.finalTitle = this.titleXForY + ' ' + this.titleProductName;
+    } else if (this.selectedFormat == 5) {
+      this.finalTitle = this.titleAmount + ' for ' + this.titleProductName;
+    } else if (this.selectedFormat == 6) {
+      this.finalTitle = 'Free ' + this.titleProductName;
+    }
   }
 
   addTags(event: MatChipInputEvent): void {
@@ -128,44 +233,34 @@ export class EditProcessedPostComponent implements OnInit {
   }
 
   async onSubmit() {
-    if (this.editForm.invalid) {
-      this.error = true;
-      this.errMsg = 'Please complete all required fields.';
-      setTimeout(() => {
-        this.error = false;
-        this.errMsg = undefined;
-      }, 2000);
-      return;
-    } else {
-      this.product.title = this.titleCtrl.value;
-      this.product.content = this.descriptionCtrl.value;
-      this.product.categories = this.categories;
-      this.product.startDate = this.datePicker.get('start').value;
-      this.product.endDate = this.datePicker.get('end').value;
-      console.log(this.product);
+    this.product.title = this.titleCtrl.value;
+    this.product.content = this.descriptionCtrl.value;
+    this.product.categories = this.categories;
+    this.product.startDate = this.datePicker.get('start').value;
+    this.product.endDate = this.datePicker.get('end').value;
+    console.log(this.product);
 
-      this.datasrv.updatePost(this.product).subscribe({
-        next: (v) => console.log(v),
-        error: (err) => {
+    this.datasrv.updatePost(this.product).subscribe({
+      next: (v) => console.log(v),
+      error: (err) => {
+        if (err.error.errors == 'Nil JSON web token') {
+          console.log('need login');
+          this.router.navigate(['/']);
+        }
+      },
+      complete: () => {
+        try {
+          this.datasrv.updateAllProductList();
+        } catch (err: any) {
           if (err.error.errors == 'Nil JSON web token') {
             console.log('need login');
             this.router.navigate(['/']);
           }
-        },
-        complete: () => {
-          try {
-            this.datasrv.updateAllProductList();
-          } catch (err: any) {
-            if (err.error.errors == 'Nil JSON web token') {
-              console.log('need login');
-              this.router.navigate(['/']);
-            }
-            return;
-          }
-          this.router.navigate(['home/processed'], {});
-        },
-      });
-    }
+          return;
+        }
+        this.router.navigate(['home/processed'], {});
+      },
+    });
   }
   deletePost(id: number) {
     const dialogData = new ConfirmationDialogModel('Delete this post?', '');
--- frontend/src/app/env.ts
+++ frontend/src/app/env.ts
@@ -1,7 +1,7 @@
 export const API_URL = 'http://localhost:3000';
 
 // export const API_URL =
-//     'https://rubyduckies-rails-backend-wmma3ffcjq-as.a.run.app';
+//   'https://rubyduckies-rails-backend-wmma3ffcjq-as.a.run.app';
 
 // export const AI_URL = 'http://localhost:8080';
 
